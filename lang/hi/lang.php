24b4c8584974bf12a0327b0afc6544b5ab1dd160
Syed-sulaimaan <sysulaimaan@gmail.com>
2021-10-16 12:41:17 -0700

    Added Hindi Language

---
+++ lang/hi/lang.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+  'plugin' => [
+    'name'        => 'शानदार गैलरी',
+    'description' => 'उत्तरदायी पॉपअप गैलरी बनाएं।'
+  ],
+  'galleries' => [
+    'name'                => 'नाम',
+    'plural'              => 'दीर्घाओं',
+    'new_gallery'         => 'नई गैलरी',
+    'create_galleries'    => 'गैलरी बनाएं',
+    'update_galleries'    => 'गैलरी अपडेट करें',
+    'manage_galleries'    => 'गैलरी प्रबंधित करें',
+    'preview_galleries'   => 'पूर्वावलोकन गैलरी',
+    'delete_confirm'      => 'क्या आप वाकई इस गैलरी को हटाना चाहते हैं?',
+    'return_to_galleries' => 'गैलरी सूची पर लौटें',
+    'photos'              => 'तस्वीरें'
+  ],
+  'magnific' => [
+    'name'        => 'गेलरी',
+    'description' => 'एक पृष्ठ में गैलरी से एक एल्बम प्रदर्शित करें।',
+    'choice'      => 'वह गैलरी चुनें जिसे आप प्रदर्शित करना चाहते हैं।',
+  ],
+  'preferences' => [
+    'show_gallery_in_nav_label'   => 'नेविगेशन में गैलरी प्रदर्शित करें',
+    'show_gallery_in_nav_comment' => 'इसे सक्षम करने से गैलरी सेटिंग पृष्ठ के बजाय नेविगेशन मेनू में दिखाई देती है।',
+  ],
+  'permissions' => [
+      'manage_galleries' => 'दीर्घाओं का प्रबंधन करें',
+    ]
+];
